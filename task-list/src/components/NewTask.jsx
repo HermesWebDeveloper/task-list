86c395edb24ae91e05b9775785d924672c55cd3d
Hermes Barbosa <hermesbarbosa9@gmail.com>
2024-10-01 20:11:05 -0300

    Iniciado componente de NewTask

---
+++ task-list/src/components/NewTask.jsx
@@ -0,0 +1,17 @@
+
+
+function NewTask () {
+
+    return(
+        <>
+            <div>
+                <form>
+                    <label>Digite uma nova tarefa:</label>
+                    <input type="text" className=""></input>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default NewTask;
--- task-list/src/main.jsx
+++ task-list/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import Index from './components'
 import './style/Index.css'
 import Header from './components/Header'
+import NewTask from './components/NewTask'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-mono'>
       <Header />  
+      <NewTask />
       <Index />
     </div>
   </StrictMode>,
