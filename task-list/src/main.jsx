14a37c9e39fae418d6d984cdd92e334e5e482fa1
Hermes Barbosa <hermes.barbosa@imobstylus.com>
2024-11-01 07:41:32 -0300

    Tirei importação errada de index.css

--- task-list/src/main.jsx
+++ task-list/src/main.jsx
@@ -2,7 +2,6 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './style/Index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
